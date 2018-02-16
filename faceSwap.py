2bf84b91c83127c00bc3e3ceffafb4e61f6673b4
Xiaojian "JJ" Deng <xjd001@gmail.com>
2018-02-15 22:08:18 -0600

    swap_inplace(): modifies the "body" input EasyImage inplace and optionally returns it
    Like swap(): but takes 2 EasyImage objects as input

--- faceSwap.py
+++ faceSwap.py
@@ -197,6 +197,74 @@ def swap(face, body, facenum = 0, bodynum = 0, predictor = None, detector = None
     output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
     return output
 
+def swap_inplace(face, body, facenum = 0, bodynum = 0, predictor = None, detector = None):
+    """
+Swaps face # facenum on the "face" image with face # bodynum on the "body" image
+and modifies the body EasyImage with the new face.
+
+Note: face and body need to be EasyImage objects! 
+    """
+
+    img1 = face.getimg()
+    img2 = body.getimg()
+    img1Warped = np.copy(img2);
+    face1 = face.detect_faces()[facenum]
+    face2 = body.detect_faces()[bodynum]    
+    points1 = face1.getpoints(predictor)
+    points2 = face2.getpoints(predictor)
+    
+    # Find convex hull
+    hull1 = []
+    hull2 = []
+
+    hullIndex = cv2.convexHull(np.array(points2), returnPoints = False)
+    #hullIndex = cv2.convexHull(np.array(points1), returnPoints = False)
+          
+    for i in range(0, len(hullIndex)):
+        hull1.append(points1[int(hullIndex[i])])
+        hull2.append(points2[int(hullIndex[i])])
+    
+    
+    # Find delanauy traingulation for convex hull points
+    sizeImg2 = img2.shape    
+    rect = (0, 0, sizeImg2[1], sizeImg2[0])
+     
+    dt = calculateDelaunayTriangles(rect, hull2)
+    
+    if len(dt) == 0:
+        quit()
+    
+    # Apply affine transformation to Delaunay triangles
+    for i in range(0, len(dt)):
+        t1 = []
+        t2 = []
+        
+        #get points for img1, img2 corresponding to the triangles
+        for j in range(0, 3):
+            t1.append(hull1[dt[i][j]])
+            t2.append(hull2[dt[i][j]])
+        
+        warpTriangle(img1, img1Warped, t1, t2)
+    
+            
+    # Calculate Mask
+    hull8U = []
+    for i in range(0, len(hull2)):
+        hull8U.append((hull2[i][0], hull2[i][1]))
+    
+    mask = np.zeros(img2.shape, dtype = img2.dtype)  
+    
+    cv2.fillConvexPoly(mask, np.int32(hull8U), (255, 255, 255))
+    
+    r = cv2.boundingRect(np.float32([hull2]))    
+    
+    center = ((r[0]+int(r[2]/2), r[1]+int(r[3]/2)))
+        
+    
+    # Clone seamlessly.
+    output = cv2.seamlessClone(np.uint8(img1Warped), img2, mask, center, cv2.NORMAL_CLONE)
+    body._img = output
+    return body
 
 if __name__ == '__main__' :
     
